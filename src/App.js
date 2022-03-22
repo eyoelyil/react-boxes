811302027b3703397674b4a07cb29b0c1203790e
Mulate Eyoel <s2200125@BC-C02GR0BRQ6LR-MacBook-Air.local>
2022-03-22 15:32:19 +0200

    boxes completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <div>
+        <h2>{props.name}</h2>
+        <p>Title : {props.title}</p>
+        <p>Age : {props.age}</p>
+        <ul>{props.lang}</ul>
+      </div>
+    </div>
+  );
+};
+
+const persons = [
+  {
+    name: "John",
+    title: "CEO",
+    age: 39,
+    languages: [
+      { name: "english", id: 1 },
+      { name: "finnish", id: 2 },
+      { name: "russian", id: 3 },
+      { name: "spanish", id: 4 },
+    ],
+  },
+  {
+    name: "Doe",
+    title: "Developer",
+    age: 33,
+    languages: [
+      { name: "english", id: 1 },
+      { name: "finnish", id: 2 },
+      { name: "russian", id: 3 },
+      { name: "spanish", id: 4 },
+    ],
+  },
+  {
+    name: "Tim",
+    title: "Designer",
+    age: 28,
+    languages: [
+      { name: "english", id: 1 },
+      { name: "finnish", id: 2 },
+      { name: "russian", id: 3 },
+      { name: "spanish", id: 4 },
+    ],
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
+      <main>
+        {persons.map((p) => (
+          <Box
+            key={p.name}
+            name={p.name}
+            title={p.title}
+            age={p.age}
+            lang={p.languages.map((lang) => (
+              <li key={lang.id}>{lang.name}</li>
+            ))}
+          />
+        ))}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
